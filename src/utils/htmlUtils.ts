af84c2fe083f2f9448b58e9d2ce6a7b410dd3673
kangtae49 <kangtae49@gmail.com>
2026-01-28 08:45:59 +0900

    img size

--- src/utils/htmlUtils.ts
+++ src/utils/htmlUtils.ts
@@ -4,15 +4,21 @@ export function replaceUrl(html: string, baseDir: string) {
   const isPackaged = window.api.appInfo.versions.isPackaged
   // const regex = /src="[^"]*\/([^"\/?]+)"/g;
   const regex = /src="[^"]*\/([^"?]+)[^"]*"/g;
+  const regexAStart = /<a href="javascript:void\(0\);">/g;
+  const regexAEnd = /<\/a>/g;
+  let newHtml = html;
   if (isPackaged) {
-    return html.replaceAll(regex, `src="${baseDir}/$1"`)
+    newHtml = newHtml.replaceAll(regex, `src="${baseDir}/$1"`)
   } else {
-    return html.replaceAll(regex, (match, p1) => {
+    newHtml = html.replaceAll(regex, (match, p1) => {
       // const encodedPath = encodeURIComponent(`${baseDir}/${p1}`);
       // return `src="http://localhost:5173/http_get?path=${encodedPath}"`
       const filePath = `${baseDir}/${p1}`
       return `src="local-resource://${filePath}"`
     })
+
     // return html.replaceAll(regex, `src="http://localhost:5173/http_get?path=${baseDir}/$1"`)
   }
+  newHtml = newHtml.replaceAll(regexAStart, '')
+  return newHtml.replaceAll(regexAEnd, '')
 }
